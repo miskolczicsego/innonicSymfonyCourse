a8dad10fe7f474050db2e2f35528ed75e4901626
miskolczicsego <miskolczicsego@gmail.com>
2016-11-20 20:47:36 +0100

    innonic symfony course 13
    - logout functionality

--- src/Blog/AdminBundle/Controller/SecurityController.php
+++ src/Blog/AdminBundle/Controller/SecurityController.php
@@ -52,4 +52,14 @@ class SecurityController extends Controller
 
     }
 
+    /**
+     * Logout action
+     *
+     * @Route("logout")
+     */
+    public function logoutAction()
+    {
+
+    }
+
 }
